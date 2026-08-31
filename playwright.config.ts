d413d62d374f7d5309b4e6a1197e44c01feb370a
matiastartara <19376841+matiastartara@users.noreply.github.com>
2026-08-31 17:55:54 -0300

    refactor: remove commented-out mobile and branded browser configurations from playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -59,25 +59,5 @@ export default defineConfig({
       name: 'api',
       testDir: './tests/api',
     },
-
-    //Test against mobile viewports. 
-    // {
-    //  name: 'Mobile Chrome',
-    //  use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
 });
